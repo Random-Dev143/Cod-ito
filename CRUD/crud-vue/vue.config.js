dfa9af838d00af015a0ad6aee1f0281dd441a5bf
Facundo Rua <facundo.rua90@gmail.com>
2023-12-01 12:59:56 -0300

    try 3

--- CRUD/crud-vue/vue.config.js
+++ CRUD/crud-vue/vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   publicPath: process.env-NODE_ENV === 'production'
-  ? '/crud-vue/'
+  ? 'Cod-ito/CRUD/crud-vue/'
   :'/',
   transpileDependencies: true,
   configureWebpack: {
