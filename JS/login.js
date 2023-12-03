0bef809f2efec71be3c93a9aff5e08e05161d243
mreinoso99 <mreinoso@blueorange.com.ar>
2023-12-03 12:09:41 -0300

    Update login.js

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+document.getElementById("login.html").addEventListener("DOMContentLoaded", function(event) {
+    event.preventDefault();
+  
+    // Obtiene los valores de usuario y contraseña
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Validación de usuario y contraseña (luego re-direcciona o da mensaje de error)
+    if (username === "admin" && password === "admin") {
+      window.location.href = "FACU, LA PAGINA PARA REDIRECCIONAR VA ACA";
+    } else {
+      alert("Usuario o contraseña inválido, volver a ingresar correctamente");
+    }
+  });
+  
