0fb9fb2dde2935ff25ccd2be72e9a26ab9cfa485
Random-Dev143 <facundo.rua90@gmail.com>
2023-12-04 16:20:04 -0300

    update login.js and login.html

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    event.preventDefault();
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
   
-    // Obtiene los valores de usuario y contraseña
-    var username = document.getElementById("staticEmail").value;
+   
+    var username = document.getElementById("email").value;
     var password = document.getElementById("inputPassword").value;
   
-    // Validación de usuario y contraseña (luego re-direcciona o da mensaje de error)
+    
     if ((username === "admin" && password === "admin") ||
         (username === "Facundo.rua90@gmail.com" && password === "123") ||
         (username === "martin.reinoso99@gmail.com" && password === "0000")) {
@@ -14,3 +16,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
         alert("Usuario o contraseña inválidos, vuelve a ingresar correctamente");
 }
   });
+});
